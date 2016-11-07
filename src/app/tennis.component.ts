e60b4e75f952d30c5e99bc2d7936f7af50daa7be
Nate Morse <morse.nate@gmail.com>
2016-11-06 22:41:24 -0500

    finished tweaks to new fsm representation

--- src/app/tennis.component.ts
+++ src/app/tennis.component.ts
@@ -6,15 +6,42 @@ import { Component } from '@angular/core';
   templateUrl: 'tennis.component.html',
   styleUrls: ['tennis.component.css']
 })
+
 export class TennisAppComponent {
-  //37215
-  state = new State({"states":{"P1 leads":{},"All":{},"P2 leads":{},"Start":{},"Advantage P1":{},"Advantage P2":{},"Deuce":{},"Game P2":{},"Game P1":{}},"trans":{"P1 leads":{"1":"Game P1","2":"Deuce"},"All":{"1":"P1 leads","2":"P2 leads"},"P2 leads":{"1":"Deuce","2":"Game P2"},"Start":{"1":"P1 leads","2":"P2 leads"},"Advantage P1":{"1":"Game P1","2":"Deuce"},"Advantage P2":{"1":"Deuce","2":"Game P2"},"Deuce":{"1":"Advantage P1","2":"Advantage P2"},"Game P2":{},"Game P1":{}},"views":[{"name":"primary","nodes":{"P1 leads":{"position":{"x":112,"y":105},"width":90},"All":{"position":{"x":300,"y":150}},"P2 leads":{"position":{"x":502,"y":106},"width":90},"Start":{"position":{"x":300,"y":70},"width":60},"n0":{"position":{"x":154,"y":250},"width":110},"n1":{"position":{"x":450,"y":250},"width":110},"n2":{"position":{"x":300,"y":250},"width":60},"n3":{"position":{"x":550,"y":300},"width":90},"n4":{"position":{"x":50,"y":300},"width":90}},"edges":{}},{"name":"kitty corner","nodes":{"n4":{"position":{"x":490,"y":50},"width":90},"n3":{"position":{"x":490,"y":250},"width":90},"n2":{"position":{"x":210,"y":150},"width":60},"n1":{"position":{"x":350,"y":200},"width":110},"n0":{"position":{"x":350,"y":100},"width":110},"Start":{"position":{"x":10,"y":150},"width":60},"P2 leads":{"position":{"x":75,"y":250},"width":90},"All":{"position":{"x":120,"y":150}},"P1 leads":{"position":{"x":75,"y":50},"width":90}},"edges":{}}]});
-  //                   { states:{a:{start_state: true}, b:{}},
-  //                    transitions:{a:{P1:"b"}, b:{P2:"a"}}});
+  title = "";
+  p1 = 0;
+  p2 = 0;
+  state = new State({"states":{"Game P1":{},"Game P2":{},"Deuce":{},"Advantage P2":{},"Advantage P1":{},"Start":{},"P2 leads":{},"All":{},"P1 leads":{}},"trans":{"Game P1":{},"Game P2":{},"Deuce":{"1":{"Advantage P1":true},"2":{"Advantage P2":true}},"Advantage P2":{"1":{"Deuce":true},"2":{"Game P2":true}},"Advantage P1":{"1":{"Game P1":true},"2":{"Deuce":true}},"Start":{"1":{"P1 leads":true},"2":{"P2 leads":true}},"P2 leads":{"1":{"All":function () { return (this.tied() && this.score('P1') < this.forty());},"Deuce":function () { return (this.tied() && this.score('P1') >= this.forty());}},"2":{"P2 leads":true,"Game P2":function () { return (this.score('P2') >= this.forty());}}},"All":{"1":{"P1 leads":true},"2":{"P2 leads":true}},"P1 leads":{"1":{"P1 leads":true,"Game P1":function () { return (this.score('P1') >= this.forty());}},"2":{"All":function () { return (this.tied() && this.score('P2') < this.forty());},"Deuce":function () { return (this.tied() && this.score('P2') >= this.forty());}}}},"views":[{"name":"primary","nodes":{"Game P1":{"position":{"x":50,"y":300},"width":90},"Game P2":{"position":{"x":550,"y":300},"width":90},"Deuce":{"position":{"x":300,"y":250},"width":60},"Advantage P2":{"position":{"x":450,"y":250},"width":110},"Advantage P1":{"position":{"x":154,"y":250},"width":110},"Start":{"position":{"x":300,"y":70},"width":60},"P2 leads":{"position":{"x":502,"y":106},"width":90},"All":{"position":{"x":300,"y":150}},"P1 leads":{"position":{"x":112,"y":105},"width":90}},"edges":{}},{"name":"kitty corner","nodes":{"Game P1":{"position":{"x":490,"y":50},"width":90},"Game P2":{"position":{"x":490,"y":250},"width":90},"Deuce":{"position":{"x":210,"y":150},"width":60},"Advantage P2":{"position":{"x":350,"y":200},"width":110},"Advantage P1":{"position":{"x":350,"y":100},"width":110},"Start":{"position":{"x":10,"y":150},"width":60},"P2 leads":{"position":{"x":75,"y":250},"width":90},"All":{"position":{"x":120,"y":150}},"P1 leads":{"position":{"x":75,"y":50},"width":90}},"edges":{}}]});
+
   point(player: string): void {
-    this.state.signal(player);
+    if (player === '1') { this.p1 += 1; }
+    if (player === '2') { this.p2 += 1; }
+    this.state.signal(player, this);
   }
 
+  tied() {return (this.p1 === this.p2);}
+
+  score(player) {
+    if (player === 'P1') { return this.p1; }
+    if (player === 'P2') { return this.p2; }
+  }
+
+  forty() {return 3;}
+
+  sayScore(score) {
+    if (score === 0) {
+      return 'Love';
+    }
+    if (score === 1) {
+      return 'Fifteen';
+    }
+    if (score === 2) {
+      return 'Thirty';
+    }
+    if (score === 3) {
+      return 'Forty';
+    }
+  }
 }
 
 class State {
@@ -36,31 +63,31 @@ class State {
       alert('The start state was not initialized in the StateMachine constructor');
     }
   }
-  signal(trans: string) {
+
+  // signal the state machine with an environment that a guard can evaluate within.
+  signal(trans: string, env) {
     let next_state = '';
+    let default_state = '';
     let possible_trans = this.fsa['trans'][this.current_state];
     if (possible_trans[trans]) {
       let pt = possible_trans[trans];
-      if (typeof pt === 'string') {
-        this.current_state = pt;
-      }
-      else {
-        for (let tn in pt) {
-          if (pt[tn].guard && eval(pt[tn].guard)) {
+      for (let tn in pt) {
+        if (typeof pt[tn] === 'boolean' && pt[tn] === true) {
+          //alert('signal '+ trans + ' to default_state ' + tn );
+          default_state = tn;
+        }
+        else {
+          if (pt[tn].call(env)) {
             next_state = tn;
-            break;
-          }
-        }
-        if (!next_state) {
-          for (let tn in pt) {
-            if (!pt[tn].guard) {
-              next_state = tn;
-              break;
-            }
           }
         }
       }
-      if (next_state) {
+      if (!next_state && default_state) {
+        //alert('using default_state ' + default_state );
+        this.current_state = default_state;
+      }
+      else if (next_state) {
+        //alert('using next_state ' + next_state );
         this.current_state = next_state;
       }
     }
@@ -70,3 +97,6 @@ class State {
     return this.current_state;
   }
 }
+
+
+//37215
